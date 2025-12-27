4da15a8ad4d86020d91b30288f7fb0019857170e
cyd <cydmdalupan@gmail.com>
2025-12-27 19:51:58 +0800

    new key

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  ENCRYPTION_KEY: 'SBAHtID19eaYzc+zFHWYNLioMW/VERgzC0FLR4qWNdM=', 
+  ENCRYPTION_KEY: '54h3128rxx06lsr3kd7e2290kqw9z10w', 
   API_BASE_URL: '/mock/api' 
 };
