be19e07659a3fde69474a6b741a870984d012868
cyd <cydmdalupan@gmail.com>
2025-12-27 19:06:00 +0800

    move folder

--- angular/angular-app/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  ENCRYPTION_KEY: 'your_encryption_key', // This should match ENCRYPTION_KEY in .env
-  API_BASE_URL: '/mock/api' // Base URL for your PHP backend
-};
---
+++ angular/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  ENCRYPTION_KEY: 'SBAHtID19eaYzc+zFHWYNLioMW/VERgzC0FLR4qWNdM=', 
+  API_BASE_URL: '/mock/api' 
+};
