e4adbb811828c04596f391b806fc25a679cb07cd
cyd <cydmdalupan@gmail.com>
2025-12-27 18:53:35 +0800

    Re-added postbuild script to correctly flatten Angular build output

--- angular/angular-app/src/environments/environment.prod.ts
+++ angular/angular-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   ENCRYPTION_KEY: 'your_encryption_key', // This should match ENCRYPTION_KEY in .env
-  API_BASE_URL: '/api' // Base URL for your PHP backend
+  API_BASE_URL: '/mock/api' // Base URL for your PHP backend
 };
